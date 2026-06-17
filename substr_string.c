d14e8850b984f81ead07b3307c28d8384a459f21
altaris33 <jeremie.laera@live.fr>
2026-06-18 00:09:37 +0200

    Substring in string: implemented a method to find a string within another string using strstr()

--- substr_string.c
+++ substr_string.c
@@ -10,8 +10,16 @@
  * Task 1 : find all occurrences of a character inside a string 
  *          and format the result as in this example:
  *          "This is a simple string"
- *              ^  ^   ^      ^                     
+ *              ^  ^   ^      ^           
+ * 
+ * Substring :
+ * Given a string: 
+ *          "This is a simple string"   
+ * Find the occurrence of a substring "simple" and format the output as follow:
+ *          "This is a simple string"
+ *                     ------
  */
+
 
 int find_all_occurrences(char str[], char c, int indexes[]) {
     int found = 0;
@@ -33,6 +41,20 @@ int find_all_occurrences(char str[], char c, int indexes[]) {
     return found;
 } 
 
+/**
+ * returns -1 if the substr wasn't found
+ * otherwise returns the starting index of the substr within str 
+ *  (i.e an offset in position between str and substr)
+ * 
+ * strstr() returns a NULL pointer if the substring wasn't found
+ *  otherwise it will return a pointer to the base address of the found char 
+ *  if it present in str
+ */
+int find_substr(char str[], char substr[]) {
+    char *p_sub = strstr(str, substr);
+    return p_sub ? p_sub - str : -1;
+}
+
 void print_occurrences(char str[], int occurrences[], int found) {
     char pattern[strlen(str) + 1]; // +1 to save a slot for the null character
     memset(pattern , ' ' , sizeof(pattern));
@@ -45,6 +67,19 @@ void print_occurrences(char str[], int occurrences[], int found) {
     printf("%s\n", pattern);
 }
 
+void print_substr(char str[], char substr[], int index) {
+    printf("Finding substring \"%s\":\n", substr);
+    printf("%s\n", str);
+    if (index >= 0)
+    {
+        for (int i = 0; i < index + strlen(substr); i++)
+        {
+            printf("%c", (i < index) ? ' ' : '-');
+        }    
+    }
+    printf("\n");
+}
+
 int main() {
 
     printf("====String Searching and Tokenizing===\n");
@@ -52,9 +87,12 @@ int main() {
     int occurrences[sizeof(str)];
 
     int found = find_all_occurrences(str, 's', occurrences);
-    printf("found: %d in %s\n", found, str);
     print_occurrences(str, occurrences, found);
 
-    printf("%s\n", strchr("test", 'e'));
+    char str_2[] = "this is a simple string";
+    char substr[] = "simple";
+    int sub_found = find_substr(str_2, substr);
+    print_substr(str_2, substr, sub_found);
+
     return 0;
 }
